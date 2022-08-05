98a7e2f8dff6c8ae26cbfd1728ef7b0a16929196
wesleynepo <wesleynepomuceno1@gmail.com>
2022-08-04 23:46:36 -0300

    feat: spanish lang

--- hello.go
+++ hello.go
@@ -2,15 +2,22 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name string, languague string) string {
     if name == "" {
         name = "World"
     }
+
+    if languague == spanish {
+        return spanishHelloPrefix + name
+    }
+
     return englishHelloPrefix + name
 }
 
 func main() {
-    fmt.Println(Hello("world"))
+    fmt.Println(Hello("world", ""))
 }
