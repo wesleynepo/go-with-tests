0bfef3e44e80cfc5d12f33c26cb87b0bd46c1fd7
wesleynepo <wesleynepomuceno1@gmail.com>
2022-08-04 23:52:40 -0300

    feat: french lang

--- hello.go
+++ hello.go
@@ -3,19 +3,26 @@ package main
 import "fmt"
 
 const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello(name string, languague string) string {
     if name == "" {
         name = "World"
     }
 
-    if languague == spanish {
-        return spanishHelloPrefix + name
+    prefix := englishHelloPrefix
+
+    switch languague {
+    case french:
+        prefix = frenchHelloPrefix
+    case spanish:
+        prefix = spanishHelloPrefix
     }
 
-    return englishHelloPrefix + name
+    return prefix + name
 }
 
 func main() {
