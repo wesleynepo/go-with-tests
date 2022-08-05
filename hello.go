aba5a5ea06d2a5b727af3f8ea100f1d71891565e
wesleynepo <wesleynepomuceno1@gmail.com>
2022-08-05 00:00:05 -0300

    refactor: move lang to a function

--- hello.go
+++ hello.go
@@ -8,21 +8,24 @@ const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
 const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string, languague string) string {
+func Hello(name string, language string) string {
     if name == "" {
         name = "World"
     }
 
-    prefix := englishHelloPrefix
+    return greetingPrefix(language) + name
+}
 
-    switch languague {
+func greetingPrefix(language string) (prefix string) {
+    switch language {
     case french:
         prefix = frenchHelloPrefix
     case spanish:
         prefix = spanishHelloPrefix
+    default:
+        prefix = englishHelloPrefix
     }
-
-    return prefix + name
+    return
 }
 
 func main() {
