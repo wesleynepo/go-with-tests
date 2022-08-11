20b65a6cb54fd75df9dc3b0c599757fecde765bd
wesleynepo <wesleynepomuceno1@gmail.com>
2022-08-11 19:20:09 -0300

    feat: pointers & errors

---
+++ wallet/wallet.go
@@ -0,0 +1,39 @@
+package wallet
+
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+    return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct{
+    balance Bitcoin
+}
+
+type Stringer interface {
+    String() string
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+    w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+    return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+    if amount > w.balance {
+        return ErrInsufficientFunds
+    }
+    w.balance -= amount
+
+    return nil
+}
